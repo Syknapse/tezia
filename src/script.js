5c0132dae3787ddc2bc75044bb0d21794e721946
Syk Houdeib <syk.houdeib@gmail.com>
2020-04-19 18:06:28 +0200

    Refactor and cleanup script
    First acceptable script.js version that I can show another person and not die of embarrassment.

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
-const sound = 0
+const sound = 0 // Synth sound (piano)
 const octave = 3
 const duration = 1
 const C_major = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-let a = 0
-let b = 0
-let interval = 1
-let round = 0
-let repetitions = 0
+let a = 0 // The position of note A to play
+let b = 0 // The position of note B to play
+let interval = 1 // The position in the scale array where not B begins (zero index). Musical interval is interval +1. 
+let cycle = 0 // Number of elapsed cycles (full repetitions of the scale)
+let repetitions = 0 // The number of repetitions loop B does before changing interval
 let timeoutClearA, timeoutClearB
-const TEMPO = 600
+const TEMPO = 500
 
 const colors = {
    C: '#ef7d7d',
@@ -20,106 +20,43 @@ const colors = {
    B: '#efbc7d',
 }
 
-/* const startLoop = scale => {
+// a & b indicate the zero index position of the note to be played in the scale array. It increases by one until we reach the end of the scale.
+// We start from interval 1. This is the equivalent of a 2nd interval in music.
+// Loop B only plays the notes beginning from the interval position. Interval 1 plays all the notes minus the first while interval 6 plays only the last note.
+// Cycle begins at 0 instead of 1 to give us one first cycle where A and B are in unison before the first cycle of shifting loops begins.
+// When the b note reaches the end of the scale we repeat starting from the interval and indicate a new cycle
+// The number of repetitions we want on each interval is in inverse relation to the length of the scale. (there are 7 notes, if interval is 3 the repetitions are 4 totaling 7)
+// When we have had the amount of cycles the repetitions indicate, we push one interval up (interval++) and reset the cycle
+
+const startLoopA = scale => {
    timeoutClearA = setTimeout(() => {
       const note = scale[a]
-      // console.log('A+++++: ', note)
       Synth.play(sound, note, octave, duration)
       applyVisuals('loop-a', note)
-      a < scale.length - 1 ? a++ : a = 0
-      startLoop(scale)
-   }, TEMPO)
-} */
-const startLoop = scale => {
-   timeoutClearA = setTimeout(() => {
-      const note = scale[a]
-      // console.log('A+++++: ', note)
-      Synth.play(sound, note, octave, duration)
-      applyVisuals('loop-a', note)
-      // a < scale.length - 1 ? a++ : a = 0
       if (a < scale.length - 1) {
          a++
       } else {
          a = 0
-         round++
+         cycle++
       }
-      startLoop(scale)
+      startLoopA(scale)
    }, TEMPO)
 }
-
-/* const startLoopB = scale => {
-   timeoutClearB = setTimeout(() => {
-      const note = scale[b]
-      console.log('B------: ', note)
-      Synth.play(sound, note, octave, duration)
-      applyVisuals('loop-b', note)
-      b < scale.length - 1 ? b++ : b = 3
-      startLoopB(scale)
-   }, TEMPO)
-} */
-
-// b is the position of the note to play. increase by one until we reach the end of the scale.
-// interval is the note interval from which to start
-// When the b note reaches the end of the scale we repeat starting from the interval and indicate a new round
-// The number of repetitions we want on each interval is in inverse relation to the length of the scale. (there are 7 notes, if interval is 3 the repetitions are 4 totaling 7)
-// When we have had the amount of rounds the repetitions indicate, we push one interval up (interval++) and reset the round
-// Problems to solve:
-// check the repetitions are right
 
 const startLoopB = scale => {
    timeoutClearB = setTimeout(() => {
       const note = scale[b]
-      // console.log('B------: ', note)
       const repetitions = scale.length - interval
-      console.log("interval, repetitions: ", interval, repetitions)
-      console.log('round----: ', round)
       Synth.play(sound, note, octave, duration)
       applyVisuals('loop-b', note)
       b < scale.length - 1 ? b++ : b = interval
-      /* if (b < scale.length - 1) {
-         b++
-      } else {
-         b = interval
-         round++
-      } */
-      if (round > repetitions) {
+      if (cycle > repetitions) {
          interval === 6 ? interval = 1 : interval++
-         
-         round = 1
+         cycle = 1
       }
       startLoopB(scale)
    }, TEMPO)
 }
-/* const startLoopB = scale => {
-   timeoutClearB = setTimeout(() => {
-      const note = scale[b]
-      // console.log('B------: ', note)
-      const repetitions = scale.length - interval
-      console.log("interval, repetitions: ", interval, repetitions)
-      console.log('round----: ', round)
-      Synth.play(sound, note, octave, duration)
-      applyVisuals('loop-b', note)
-      if (b < scale.length - 1) {
-         b++
-      } else {
-         b = interval
-         round++
-      }
-      if (round > repetitions) {
-         interval === 6 ? interval = 1 : interval++
-         
-         round = 1
-      }
-      startLoopB(scale)
-   }, TEMPO)
-} */
-// repetitions--
-         /* if (interval === 6) {
-            interval = 1
-            repetitions = scale.length - 1
-         } else {
-            interval++
-         } */
 
 const applyVisuals = (id, note) => {
    const el = document.getElementById(id)
@@ -127,13 +64,10 @@ const applyVisuals = (id, note) => {
    el.innerText = note
 }
 
-const init = () => {
-   console.log('-----init-------')
-   Synth.setVolume(0.50)
-   // Synth.play(sound, 'C', '5', '4')
+const start = () => {
    stop()
-   // repetitions = C_major.length - 1 
-   startLoop(C_major)
+   Synth.setVolume(0.50)
+   startLoopA(C_major)
    startLoopB(C_major)
 }
 
@@ -143,14 +77,13 @@ const stop = () => {
    a = 0
    b = 0
    interval = 1
-   round = 0
+   cycle = 0
 }
 
 const button = document.getElementById('start')
 let on = false
 const toggleMusic = () => {
    on = !on
-   console.log('toggle', on)
-   on ? init() : stop()
+   on ? start() : stop()
 }
 button.addEventListener('click', toggleMusic)
