5c292c1098014c2b03b507cc833ebf47ec9b56c0
Syk Houdeib <syk.houdeib@gmail.com>
2020-05-11 19:30:33 +0200

    Grace mode refactored into an object

--- src/chromatic_random.js
+++ src/chromatic_random.js
@@ -13,94 +13,94 @@ let timeoutClearA, timeoutClearB, timeoutClearC
 const TEMPO = 500
 
 const startLoop = () => {
-   timeoutClearA = setTimeout(() => {
-       const note = currentScale[a]
-       console.log('A+++++: ', note)
+  timeoutClearA = setTimeout(() => {
+    const note = currentScale[a]
+    console.log('A+++++: ', note)
     Synth.play(sound, note, octave, duration)
-   if (a < NUMBER_OF_NOTES - 1) {
-      a++ 
-   } else {
+    if (a < NUMBER_OF_NOTES - 1) {
+      a++
+    } else {
       a = 0
       cycle++
       console.log(`__________ cycle: ${cycle} ___________`)
-   }
+    }
     startLoop()
-   }, TEMPO)
+  }, TEMPO)
 }
 
 const startLoopB = () => {
-   timeoutClearB = setTimeout(() => {
-       const note = shuffledCurrentScale[b]
-       console.log('B------: ', note)
+  timeoutClearB = setTimeout(() => {
+    const note = shuffledCurrentScale[b]
+    console.log('B------: ', note)
     Synth.play(sound, note, octave, duration)
-   if ( b < NUMBER_OF_NOTES - 1) {
+    if (b < NUMBER_OF_NOTES - 1) {
       b++
-   } else {
+    } else {
       b = 0
       if (cycle > repetitions) {
-         setCurrentScales()
-         cycle = 1
-         console.log('==========> new current scale: ', currentScale)
+        setCurrentScales()
+        cycle = 1
+        console.log('==========> new current scale: ', currentScale)
       } else {
-         shuffle(shuffledCurrentScale)
-         console.log('>>> reshuffle B scale: ', shuffledCurrentScale)
+        shuffle(shuffledCurrentScale)
+        console.log('>>> reshuffle B scale: ', shuffledCurrentScale)
       }
-   }
+    }
     startLoopB()
-   }, TEMPO)
+  }, TEMPO)
 }
 
 // Create a new scale by taking the specified number of random notes from the provided scale
-const createRandomScale = scale => scale.sort(() => 0.5 - Math.random()).slice(0, NUMBER_OF_NOTES) 
+const createRandomScale = scale => scale.sort(() => 0.5 - Math.random()).slice(0, NUMBER_OF_NOTES)
 const setCurrentScales = () => {
-   currentScale = createRandomScale(chromaticScale)
-   shuffledCurrentScale = shuffle([...currentScale]) 
+  currentScale = createRandomScale(chromaticScale)
+  shuffledCurrentScale = shuffle([...currentScale])
 }
 
 const shuffle = array => {
-   var currentIndex = array.length, temporaryValue, randomIndex;
- 
-   // While there remain elements to shuffle...
-   while (0 !== currentIndex) {
- 
-     // Pick a remaining element...
-     randomIndex = Math.floor(Math.random() * currentIndex);
-     currentIndex -= 1;
- 
-     // And swap it with the current element.
-     temporaryValue = array[currentIndex];
-     array[currentIndex] = array[randomIndex];
-     array[randomIndex] = temporaryValue;
-   }
- 
-   return array;
- }
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 const init = () => {
-   console.log('-----init-------')
-   stop()
-   Synth.setVolume(0.40)
-   setCurrentScales()
-   console.log('currentScale', currentScale)
-   console.log('shuffledCurrentScale', shuffledCurrentScale)
-   startLoop()
-   timeoutClearC = setTimeout(() => startLoopB(), 250)
+  console.log('-----init-------')
+  stop()
+  Synth.setVolume(0.40)
+  setCurrentScales()
+  console.log('currentScale', currentScale)
+  console.log('shuffledCurrentScale', shuffledCurrentScale)
+  startLoop()
+  timeoutClearC = setTimeout(() => startLoopB(), 250)
 }
 
 const stop = () => {
-   clearTimeout(timeoutClearA)
-   clearTimeout(timeoutClearB)
-   clearTimeout(timeoutClearC)
-   a = 0
-   b = 0
-   cycle = 1
+  clearTimeout(timeoutClearA)
+  clearTimeout(timeoutClearB)
+  clearTimeout(timeoutClearC)
+  a = 0
+  b = 0
+  cycle = 1
 }
 
 const button = document.getElementById('start')
 let on = false
 const toggleMusic = () => {
-   on = !on
-   console.log('toggle', on)
-   on ? init() : stop()
+  on = !on
+  console.log('toggle', on)
+  on ? init() : stop()
 }
 button.addEventListener('click', toggleMusic)
---
+++ src/modules/Grace/Grace.js
@@ -0,0 +1,88 @@
+import { applyVisuals, updateInfoPanel, displayInfoPanel, shuffleArray } from '../../utils/index.js'
+
+const Grace = {
+  sound: 0,
+  octave: 3,
+  duration: 1,
+  chromaticScale: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
+  NUMBER_OF_NOTES: 7, // Arbitrary number of notes in the scale we create
+  repetitions: 7, // Arbitrary number of times the same A scale repeats
+  currentScale: [],
+  shuffledCurrentScale: [],
+  a: 0, // position note a
+  b: 0, // position note b
+  cycle: 1,
+  timeoutClearA: null,
+  timeoutClearB: null,
+  timeoutClearC: null,
+  TEMPO: 500,
+
+  startLoopA() {
+    this.timeoutClearA = setTimeout(() => {
+      const note = this.currentScale[this.a]
+      console.log('A+++++: ', note)
+      Synth.play(this.sound, note, this.octave, this.duration)
+      if (this.a < this.NUMBER_OF_NOTES - 1) {
+        this.a++
+      } else {
+        this.a = 0
+        this.cycle++
+        console.log(`__________ cycle: ${this.cycle} ___________`)
+      }
+      this.startLoopA()
+    }, this.TEMPO)
+  },
+
+  startLoopB() {
+    this.timeoutClearB = setTimeout(() => {
+      const note = this.shuffledCurrentScale[this.b]
+      console.log('B------: ', note)
+      Synth.play(this.sound, note, this.octave, this.duration)
+      if (this.b < this.NUMBER_OF_NOTES - 1) {
+        this.b++
+      } else {
+        this.b = 0
+        if (this.cycle > this.repetitions) {
+          this.setCurrentScales()
+          this.cycle = 1
+          console.log('==========> new current scale: ', this.currentScale)
+        } else {
+          shuffleArray(this.shuffledCurrentScale)
+          console.log('>>> reshuffle B scale: ', this.shuffledCurrentScale)
+        }
+      }
+      this.startLoopB()
+    }, this.TEMPO)
+  },
+
+  // Create a new scale by taking the specified number of random notes from the provided scale
+  createRandomScale(scale) {
+    return scale.sort(() => 0.5 - Math.random()).slice(0, this.NUMBER_OF_NOTES)
+  },
+  setCurrentScales() {
+    this.currentScale = this.createRandomScale(this.chromaticScale)
+    this.shuffledCurrentScale = shuffleArray([...this.currentScale])
+  },
+
+  start() {
+    console.log('-----init-------')
+    this.stop()
+    Synth.setVolume(0.40)
+    this.setCurrentScales()
+    console.log('currentScale', this.currentScale)
+    console.log('shuffledCurrentScale', this.shuffledCurrentScale)
+    this.startLoopA()
+    this.timeoutClearC = setTimeout(() => this.startLoopB(), this.TEMPO / 2)
+  },
+
+  stop() {
+    clearTimeout(this.timeoutClearA)
+    clearTimeout(this.timeoutClearB)
+    clearTimeout(this.timeoutClearC)
+    this.a = 0
+    this.b = 0
+    this.cycle = 1
+  },
+}
+
+export default Grace
